a82882e1ad82a6864d9394fe175e138bfa99565b
modderme123 <modderme123@gmail.com>
2018-02-20 11:21:27 -0800

    movement

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,39 @@ use rand::random;
 use piston_window::*;
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", (640, 480))
+    let mut window: PistonWindow = WindowSettings::new("Miner!", (640, 480))
         .exit_on_esc(true)
         .build()
-        .unwrap_or_else(|e| panic!("Failed to build PistonWindow: {}", e));
+        .unwrap();
     let mut you = (50.0, 50.0);
+    let mut clicking = false;
+    let mut cursor = (0.0, 0.0);
     while let Some(e) = window.next() {
+        e.mouse_cursor(|x, y| cursor = (x, y));
+        if let Some(Button::Mouse(_)) = e.press_args() {
+            clicking = true
+        }
+        if let Some(Button::Mouse(_)) = e.release_args() {
+            clicking = false
+        }
+
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            match key {
+                Key::Up => you.1 -= 10.0,
+                Key::Down => you.1 += 10.0,
+                Key::Left => you.0 -= 10.0,
+                Key::Right => you.0 += 10.0,
+                _ => (),
+            };
+        }
         window.draw_2d(&e, |c, g| {
             clear([0.5, 1.0, 0.5, 1.0], g);
-            rectangle([1.0, 1.0, 0.5, 1.0],[you.0,you.1,50.0,50.0],c.transform, g);
+            rectangle(
+                [if clicking { 1.0 } else { 0.0 }, 1.0, 0.5, 1.0],
+                [you.0, you.1, 50.0, 50.0],
+                c.transform,
+                g,
+            );
         });
     }
-    println!("{} Hello world, I LOST THE GAME!", random::<i64>());
 }
