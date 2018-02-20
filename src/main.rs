a95e3db1f7c01e8359b14bf473b62a9fb65725b0
modderme123 <modderme123@gmail.com>
2018-02-20 11:10:25 -0800

    draw

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ fn main() {
         .exit_on_esc(true)
         .build()
         .unwrap_or_else(|e| panic!("Failed to build PistonWindow: {}", e));
-    let mut you = (50, 50);
+    let mut you = (50.0, 50.0);
     while let Some(e) = window.next() {
-        window.draw_2d(&e, |_c, g| {
+        window.draw_2d(&e, |c, g| {
             clear([0.5, 1.0, 0.5, 1.0], g);
+            rectangle([1.0, 1.0, 0.5, 1.0],[you.0,you.1,50.0,50.0],c.transform, g);
         });
     }
     println!("{} Hello world, I LOST THE GAME!", random::<i64>());
